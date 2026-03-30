5d2aa2dc40715612834ac0d97211fc0a168be388
EmrieAlis8519998 <eea8519998@lpssonline.com>
2026-03-30 17:04:21 +0000

    todo 3 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -38,6 +38,11 @@ var init = function (window) {
 
 
         // TODO 3 : Call the drawCircle() function
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
 
 
 
